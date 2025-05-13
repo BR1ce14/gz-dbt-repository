72647b31bf789d5385e558a7382f96230a491aee
Brice Verola <briceverola@gmail.com>
2025-05-13 13:39:47 +0000

    joined sales and products tables (margin and purchase cost calculation)

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,14 @@
+SELECT
+    sales.orders_id,
+    sales.products_id,
+    sales.quantity,
+    products.purchase_price,
+    sales.revenue,
+    sales.quantity * products.purchase_price as purchase_cost,
+    ROUND(sales.revenue - (sales.quantity * products.purchase_price),2) as margin
+FROM 
+    {{ ref("stg_raw__sales") }} AS sales
+LEFT JOIN 
+    {{ ref("stg_raw__product") }} AS products
+ON 
+    sales.products_id = products.products_id
