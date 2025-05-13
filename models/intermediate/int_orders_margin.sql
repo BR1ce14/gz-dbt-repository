7536bf6bb4faf11cb082d0ed8389d1e568d5ce9f
Brice Verola <briceverola@gmail.com>
2025-05-13 13:55:11 +0000

    margin per order

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,11 +1,13 @@
 SELECT
     orders_id,
-    max(date_date) AS date_date,
-    SUM(revenue) AS revenu,
-    SUM(quantity) AS quantity,
-    SUM(purchase_cost) AS purchase_cost,
-    SUM(margin) AS margin
+    MAX(date_date) AS date_date,
+    ROUND(SUM(revenue),2) AS revenu,
+    ROUND(SUM(quantity)) AS quantity,
+    ROUND(SUM(purchase_cost),2) AS purchase_cost,
+    ROUND(SUM(margin),2) AS margin
 FROM
     {{ ref("int_sales_margin") }}
 GROUP BY 
     orders_id
+ORDER BY
+    orders_id DESC
